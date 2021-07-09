f0ffbe4f1a0f47fc099fcb571b2adbd51d99ac49
TheKeineAhnung <fireball73@gmx.de>
2021-07-09 21:14:12 +0200

    ✨added file

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
+/*function correctWindowDivHeight() {
+  var div = document.getElementById("menu-iframe").srcdoc;
+  console.log(document.getElementsByTagName("div"));
+  console.log(div);
+  var iframe = document.getElementById("menu-iframe");
+  var iframeHeight = getComputedStyle(iframe).height;
+  div.style.height = iframeHeight;
+  console.log(div.style.height);
+}
 
+window.onload = correctWindowDivHeight();
+window.addEventListener("resize", correctWindowDivHeight);*/
