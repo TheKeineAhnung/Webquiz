05db0079c664011bca6d186bfbb3b8f8369786a9
TheKeineAhnung <fireball73@gmx.de>
2021-07-10 18:32:04 +0200

    ✨ basic trigger for selection

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -18,13 +18,11 @@ localStorage.setItem("round", "0");
 async function displayQuestion(allData) {
   for (i of allData) {
     if (Number(localStorage.getItem("round")) <= allData.length - 1) {
-      console.log("!same");
       var questionArea = document.getElementById("question");
       var answerAreaOne = document.getElementsByClassName("answer-1")[0];
       var answerAreaTwo = document.getElementsByClassName("answer-2")[0];
       var answerAreaThree = document.getElementsByClassName("answer-3")[0];
       var answerAreaFour = document.getElementsByClassName("answer-4")[0];
-      console.error(localStorage.getItem("round"));
       questionArea.innerHTML =
         "<h1 class='h1-question'>" +
         allData[localStorage.getItem("round")].question +
@@ -46,12 +44,34 @@ async function displayQuestion(allData) {
         allData[localStorage.getItem("round")].answers[3] +
         "</button>";
       await sleep(3000);
-      console.warn("after");
+      console.log("change");
       localStorage.setItem("round", Number(localStorage.getItem("round")) + 1);
+      if (Number(localStorage.getItem("round")) > allData.length - 1) {
+        localStorage.setItem(
+          "round",
+          Number(localStorage.getItem("round") - 1)
+        );
+      }
     } else {
-      console.log("same");
     }
   }
+}
+
+function answer(selectedAnswer) {
+  $.ajax({
+    url: "../data/question.json",
+    dataType: "json",
+    type: "GET",
+    success: function (r) {
+      if (
+        selectedAnswer == r[Number(localStorage.getItem("round"))].correctAnswer
+      ) {
+        console.log("correct");
+      } else {
+        console.log("incorrect");
+      }
+    },
+  });
 }
 
 loadFile();
