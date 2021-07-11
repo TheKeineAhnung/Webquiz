9d695d86c7ade8978e1e0d60667e209890744494
TheKeineAhnung <fireball73@gmx.de>
2021-07-11 17:40:10 +0200

    ✨ correct displaying for questions

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -1,3 +1,5 @@
+localStorage.setItem("round", "0");
+
 async function loadFile() {
   $.ajax({
     url: "../data/question.json",
@@ -9,69 +11,72 @@ async function loadFile() {
   });
 }
 
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-localStorage.setItem("round", "0");
-
-async function displayQuestion(allData) {
-  for (i of allData) {
-    if (Number(localStorage.getItem("round")) <= allData.length - 1) {
-      var questionArea = document.getElementById("question");
-      var answerAreaOne = document.getElementsByClassName("answer-1")[0];
-      var answerAreaTwo = document.getElementsByClassName("answer-2")[0];
-      var answerAreaThree = document.getElementsByClassName("answer-3")[0];
-      var answerAreaFour = document.getElementsByClassName("answer-4")[0];
-      questionArea.innerHTML =
-        "<h1 class='h1-question'>" +
-        allData[localStorage.getItem("round")].question +
-        "</h1>";
-      answerAreaOne.innerHTML =
-        "<button class='button-answer'>" +
-        allData[localStorage.getItem("round")].answers[0] +
-        "</button>";
-      answerAreaTwo.innerHTML =
-        "<button class='button-answer'>" +
-        allData[localStorage.getItem("round")].answers[1] +
-        "</button>";
-      answerAreaThree.innerHTML =
-        "<button class='button-answer'>" +
-        allData[localStorage.getItem("round")].answers[2] +
-        "</button>";
-      answerAreaFour.innerHTML =
-        "<button class='button-answer'>" +
-        allData[localStorage.getItem("round")].answers[3] +
-        "</button>";
-      await sleep(3000);
-      console.log("change");
-      localStorage.setItem("round", Number(localStorage.getItem("round")) + 1);
-      if (Number(localStorage.getItem("round")) > allData.length - 1) {
-        localStorage.setItem(
-          "round",
-          Number(localStorage.getItem("round") - 1)
-        );
-      }
-    } else {
-    }
-  }
-}
-
-function answer(selectedAnswer) {
+async function next() {
   $.ajax({
     url: "../data/question.json",
     dataType: "json",
     type: "GET",
     success: function (r) {
+      localStorage.setItem("round", Number(localStorage.getItem("round")) + 1);
+      reset();
+      displayQuestion(r);
+    },
+  });
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function displayQuestion(allData) {
+  var questionArea = document.getElementById("question");
+  var answerAreaOne = document.getElementsByClassName("answer-1-span")[0];
+  var answerAreaTwo = document.getElementsByClassName("answer-2-span")[0];
+  var answerAreaThree = document.getElementsByClassName("answer-3-span")[0];
+  var answerAreaFour = document.getElementsByClassName("answer-4-span")[0];
+  questionArea.innerHTML =
+    "<h1 class='h1-question'>" +
+    allData[localStorage.getItem("round")].question +
+    "</h1>";
+  answerAreaOne.innerHTML = allData[localStorage.getItem("round")].answers[0];
+  answerAreaTwo.innerHTML = allData[localStorage.getItem("round")].answers[1];
+  answerAreaThree.innerHTML = allData[localStorage.getItem("round")].answers[2];
+  answerAreaFour.innerHTML = allData[localStorage.getItem("round")].answers[3];
+}
+
+function answer(selectedAnswer) {
+  var options = document.getElementsByClassName("answer-sub-div");
+  for (i of options) {
+    i.classList.add("locked");
+  }
+  $.ajax({
+    url: "../data/question.json",
+    dataType: "json",
+    type: "GET",
+    success: function (r) {
+      var correct = document.getElementsByClassName("answer-" + selectedAnswer);
       if (
-        selectedAnswer == r[Number(localStorage.getItem("round"))].correctAnswer
+        selectedAnswer ==
+          r[Number(localStorage.getItem("round"))].correctAnswer &&
+        correct[0].classList[correct.length - 1] != "locked"
       ) {
         console.log("correct");
+        correct[0].classList.add("correct");
       } else {
         console.log("incorrect");
+        correct[0].classList.add("false");
       }
     },
   });
+}
+
+function reset() {
+  var options = document.getElementsByClassName("answer-sub-div");
+  for (i of options) {
+    i.classList.remove("locked");
+    i.classList.remove("correct");
+    i.classList.remove("false");
+  }
 }
 
 loadFile();
